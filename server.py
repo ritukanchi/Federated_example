6b26295290d9e0ade4eaaefbeb4fe5d675d46a08
Teesha12 <135981980+Teesha12@users.noreply.github.com>
2024-04-10 00:30:53 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,42 +1,41 @@
-import sys #variables used by interpreter to interact
-import numpy as np #support for matrices and arrays 
-import flwr as fl #frameowkr for FL 
+from typing import List, Tuple
 
-class SaveModelStrategy(fl.server.strategy.FedAvg): #strategy for fedaverae
-    def aggregate_fit( #
-        self,
-        rnd,
-        results,
-        failures
-    ):
-        aggregated_weights = super().aggregate_fit(rnd, results, failures)
-        if aggregated_weights is not None:
-            # Save aggregated_weights
-            print(f"Saving round {rnd} aggregated_weights...")
-            np.savez(f"round-{rnd}-weights.npz", *aggregated_weights)
-        return aggregated_weights
+from flwr.server import ServerApp, ServerConfig
+from flwr.server.strategy import FedAvg
+from flwr.common import Metrics
 
-def main():
-    if len(sys.argv) != 2:
-        print("abey bhidu run: python(python3) pythonfile.py <port no>")
-        sys.exit(1)
 
-    try:
-        port = int(sys.argv[1])
-    except ValueError:
-        print("Error: Port must be an integer")
-        sys.exit(1)
+# Define metric aggregation function
+def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
+    # Multiply accuracy of each client by number of examples used
+    accuracies = [num_examples * m["accuracy"] for num_examples, m in metrics]
+    examples = [num_examples for num_examples, _ in metrics]
 
-    # Create strategy and run server
-    strategy_MINE = SaveModelStrategy()
-    server_address = f"localhost:{port}"
-    config = fl.server.ServerConfig(num_rounds=3)
-    fl.server.start_server(
-        server_address=server_address,
+    # Aggregate and return custom metric (weighted average)
+    return {"accuracy": sum(accuracies) / sum(examples)}
+
+
+# Define strategy
+strategy = FedAvg(evaluate_metrics_aggregation_fn=weighted_average)
+
+
+# Define config
+config = ServerConfig(num_rounds=3)
+
+
+# Flower ServerApp
+app = ServerApp(
+    config=config,
+    strategy=strategy,
+)
+
+
+# Legacy mode
+if __name__ == "__main__":
+    from flwr.server import start_server
+
+    start_server(
+        server_address="0.0.0.0:8080",
         config=config,
-        grpc_max_message_length=1024*1024*1024,
-        strategy=strategy_MINE
+        strategy=strategy,
     )
-
-if __name__ == "_main_":
-    main()
