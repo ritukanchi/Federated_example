cc7a0ed0649b8a189534e3d40eaaa399e3bf1e4c
teesha <teeshamshah@gmail.com>
2024-04-10 00:33:46 +0530

    updated

--- client.py
+++ client.py
@@ -31,8 +31,27 @@ class FlowerClient(fl.client.NumPyClient):
         return loss, len(x_test), {"accuracy": accuracy}
 
 # Start Flower client
-fl.client.start_numpy_client(
-        server_address="localhost:"+str(sys.argv[1]), 
-        client=FlowerClient(), 
-        grpc_max_message_length = 1024*1024*1024
-)
+def main():
+    if len(sys.argv) != 2:
+        print("abey bhidu run: python(python3) pythonfile.py <port no>")
+        sys.exit(1)
+
+    try:
+        port = int(sys.argv[1])
+    except ValueError:
+        print("Error: Port must be an integer")
+        sys.exit(1)
+
+    # Create strategy and run server
+    strategy_MINE = SaveModelStrategy()
+    server_address = f"localhost:{port}"
+    config = fl.server.ServerConfig(num_rounds=3)
+    fl.server.start_server(
+        server_address=server_address,
+        config=config,
+        grpc_max_message_length=1024*1024*1024,
+        strategy=strategy_MINE
+    )
+
+if __name__ == "_main_":
+    main()
